d78ff6a75f298120c4c87536e8bab5fab3e887a5
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2022-10-05 14:15:28 +0000

    final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
-//Variabler för att representera nuvarande knock-out-number samt Score
+/**
+ * Declare variable for start score and number
+ */
 var pickedKON = null; 
 var score = 0;
 
-//De fyra möjliga nock-out siffrorna som spelaren kan välja
+/**
+ * Declare variables for each button/dice
+ */
 var button6 = document.getElementById('button6');
 
 var button7 = document.getElementById('button7');
@@ -11,18 +15,23 @@ var button8 = document.getElementById('button8');
 
 var button9 = document.getElementById("button9");
 
-
-//Knapp för att trigga "slå tärningarna och generera två stycken slumpvalda tal mellan 1-6"
+/**
+ * Button så roll two random numbers between 1-6
+ */
 var rollDiceButton = document.getElementById('rollDice');
 
-//Här lyssnar vi efter ett klick för varje knapp och skickar in i funktionen pickKnockOutNumber, färglgga den röd och göra de andra "disabled"
+/**
+ * Add eventlistener to trigger pickKnockOutNumber function
+ */
 button6.addEventListener('click', () => pickKnockOutNumber(6));
 button7.addEventListener('click', () => pickKnockOutNumber(7));
 button8.addEventListener('click', () => pickKnockOutNumber(8));
 button9.addEventListener('click', () => pickKnockOutNumber(9));
 rollDiceButton.addEventListener('click', rollDice);
 
-//Funktionen som gör alla knappar disabled, färglägger knockout-numret samt gör "roll dice" klickbar.
+/**
+ * Function to disable all buttons, set color for knock-out nr and make "roll" button klickable
+ */
 function pickKnockOutNumber(knockOutNumber) {
   button6.disabled = true;
   button7.disabled = true;
@@ -31,11 +40,13 @@ function pickKnockOutNumber(knockOutNumber) {
 
   document.getElementById('button'+knockOutNumber).style.backgroundColor = "red";
   document.getElementById('rollDice').disabled = false;
-  //säger att pickedKON är lika med KnockOutNumber
+
   pickedKON = knockOutNumber;
 }
 
-//funktionen som slår tärningarna 1 och 2
+/**
+ * Function to role random number for each roll number
+ */
 function rollDice() {
   var randomNumber1 = getRandomNumber(1,6);
   document.getElementById('dice_1').innerHTML = randomNumber1;
@@ -45,11 +56,15 @@ function rollDice() {
 
   console.log(sum === pickedKON)
   if (sum === pickedKON) {
-    //skapar alert med scoren som spelaren fick och gömmer allt från sidan
-    alert(`Din Score: ${score}`);
+    /**
+     * Trigger end game alert
+     */
+    alert(`GAME OVER: Your Score: ${score}`);
     document.getElementById('gamePage').style.display = 'none';
     
-    //skapar knapp för att börja om (refresh sida)
+    /**
+     * Create button to start over and play again
+     */
     var newGame = document.createElement("button");
     newGame.innerHTML = "Play again!";
     var body = document.getElementById('body');
@@ -59,14 +74,18 @@ function rollDice() {
       window.location.reload();
     }
 
-    //lägger på siffror som slås med tärningarna på scoren
+/**
+ * add score to current score nr
+ */
   } else {
     score = score + sum;
     document.getElementById('score').innerHTML = score;
   }
 }
 
-
+/**
+ * calculate the sum of dices into score
+ */
 //summerar tärningsvärderna
 function sumOfDices(dice1, dice2) {
   var sum =  dice1 + dice2;
@@ -74,7 +93,9 @@ function sumOfDices(dice1, dice2) {
   return sum;
 }
 
-//Hitta slumpmässigt nummer mellan 1-6
+/**
+ * Generate random number between 1-6
+ */
 function getRandomNumber(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
